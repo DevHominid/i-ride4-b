0e56a941a1f69d1f945ca94d1863bf7c2c7b6022
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-05 23:18:03 -0700

    init keystone

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import logger from './logger';
+import keystone from 'keystone';
 
 // Config env vars
 dotenv.config();
@@ -11,6 +12,33 @@ dotenv.config();
 // Init app
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Init keystone
+keystone.init({
+  'name': 'i-Ride4',
+  'favicon': '../public/favicons/favicon.ico',
+  'static': ['public'],
+  'auto update': true,
+  'cors allow origin': true,
+  'cors allow methods': 'GET,OPTIONS,POST',
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': 'testkeystonecookiesecret',
+  'trust proxy': true
+});
+
+if (process.env.MONGODB) {
+  keystone.set('mongo', process.env.MONGODB);
+}
+
+if (process.env.HOST) {
+  keystone.set('host', process.env.HOST);
+}
+
+if (process.env.PORT) {
+  keystone.set('port', process.env.PORT)
+}
 
 // Use Gzip compression
 app.use(compression());
@@ -35,6 +63,11 @@ app.use(morgan('dev', {
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Set keystone nav
+keystone.set('nav', {
+  'users': ['User']
+});
 
 // Handle errors
 app.use((err, req, res, next) => {
