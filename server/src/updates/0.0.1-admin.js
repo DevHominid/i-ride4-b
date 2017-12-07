999bc2a40cbbbc904248fc468b2b4900fbea5247
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-07 00:35:13 -0700

    add annotations

--- server/src/updates/0.0.1-admin.js
+++ server/src/updates/0.0.1-admin.js
@@ -14,6 +14,7 @@ import keystone from 'keystone';
  exports = module.exports = function(done) {
 
     new User.model({
+        // TODO : change credentials 
         name: { first: 'Admin', last: 'User' },
         email: 'dylan@devhominid.com',
         password: '123456',
