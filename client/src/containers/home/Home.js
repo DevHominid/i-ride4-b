547b47fd708a8d07eb4edf0cc95e34c0145ee839
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-08 15:54:52 -0700

    style index container

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import coverImg from '../../images/index-cover.jpg';
 
 import './Home.css';
 
 const Home = () => (
   <section>
-    <p>Home.</p>
+
   </section>
 );
 
