3680bc6a0213c98fa485e92847eb6e55823808e9
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-08 12:52:09 -0700

    refactor component and update tests

--- client/src/components/button-nav/ButtonNav.js
+++ client/src/components/button-nav/ButtonNav.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './ButtonNav.css';
 
-const ButtonNav = () => (
-  <button className='btn-nav'>
-    <span></span>
-    <span></span>
-    <span></span>
-  </button>
-);
+class ButtonNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonActive: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(target) {
+    console.log(target);
+    if (this.state.buttonActive) {
+      target.className = 'btn-nav';
+    } else {
+      target.className = 'btn-nav active';
+    }
+
+    const buttonState = !this.state.buttonActive;
+    this.setState({
+      buttonActive: buttonState
+    });
+  }
+
+  render() {
+    return (
+      <button id='btn-nav' className='btn-nav' onClick={(e) => this.handleClick(e.currentTarget)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    );
+  }
+};
 
 export default ButtonNav;
--- client/src/components/button-nav/ButtonNav.test.js
+++ client/src/components/button-nav/ButtonNav.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import ReactTestUtils from 'react-dom/test-utils';
+import sinon from 'sinon';
 
 import ButtonNav from './ButtonNav';
 
@@ -11,8 +11,23 @@ describe('<ButtonNav />', () => {
     expect(wrapper.length).to.equal(1);
   });
 
+  it('renders an `#btn-nav`', () => {
+    const wrapper = shallow(<ButtonNav />);
+    expect(wrapper.find('#btn-nav')).to.have.length(1);
+  });
+
   it('renders three <span> elements', () => {
     const wrapper = shallow(<ButtonNav />);
     expect(wrapper.find('span')).to.have.length(3);
+  });
+
+  it('changes state onClick', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow((
+      <ButtonNav onButtonClick={onButtonClick} />
+    ));
+    const mockEvent = { currentTarget: {} }
+    wrapper.find('#btn-nav').simulate('click', mockEvent);
+    expect(wrapper.state().buttonActive).to.equal(true);
   });
 });
