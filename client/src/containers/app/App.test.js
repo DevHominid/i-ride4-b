46ded150688feef4cd7826ddec0adcb246249b5c
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-07 22:17:50 -0700

    swap out jest for mocha, setup testing

--- client/src/containers/app/App.test.js
+++ client/src/containers/app/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('renders without imploding', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.length).to.equal(1);
+  });
 });
