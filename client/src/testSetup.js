1c3d88e73077cb2f52172e796871e6399c0a18f6
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-08 00:19:19 -0700

    install jsdom and config

--- client/src/testSetup.js
+++ client/src/testSetup.js
@@ -1,4 +1,26 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+const { JSDOM } = require('jsdom');
 
 configure({ adapter: new Adapter() });
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+}
+
+// Store DOM and the window in global scope for React to access
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
