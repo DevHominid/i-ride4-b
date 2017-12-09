5ca3a6d398b97ed9823fbc3302f2b19b2fe59d73
Dylan Ross Ludwick <dylan@devhominid.com>
2017-12-09 03:35:06 -0700

    add heading

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -6,6 +6,10 @@ import './Home.css';
 
 const Home = () => (
   <section className='section-home'>
+    <div className='heading-block'>
+      <h1>What Do You</h1>
+      <h1>Ride For?</h1>
+    </div>
     <div className='panel-module'>
       <HomePanel panelClass={'panel panel-a'} />
       <HomePanel panelClass={'panel panel-b'} />
